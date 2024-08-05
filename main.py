994135a966aa640ee57acedc394efdbe4cafea75
KeroDeKroma <kerodekroma@gmail.com>
2024-08-05 01:06:17 -0500

    removing unnecessary import lines

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import asyncio
 import pygame
 import sys
 from single_square import SingleSquare
-
-import os
-# Insert the parent directory of `py_thumbpad` into `sys.path`
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from py_thumbpad import PyThumbPad
-
 
 WIDTH, HEIGHT = 800, 600
 

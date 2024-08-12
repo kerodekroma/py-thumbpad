cca6cd07f8e5c5514d6855c4ea114daf5f4353aa
KeroDeKroma <kerodekroma@gmail.com>
2024-08-11 23:10:55 -0500

    feat: adding github actions to build/deploy to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py_thumbpad",
-    version="0.1.0",
+    version="0.1.2",
     author="kerodekroma",
     author_email="kerodekroma@gmail.com",
     description="A virtual thumb pad for directional input, featuring a central donut shape and a movable button pad.",
